6fb4ad822d108d392dd3fea5f22c8c1a65097950
kaznak <kaznak.at.work@gmail.com>
2018-11-10 12:03:02 +0900

    answer(5): simplified

--- answer.kaznak/5.types.hs
+++ answer.kaznak/5.types.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE ScopedTypeVariables #-}
 
 import Data.Functor
 
@@ -8,15 +7,6 @@ main = do
   years        <- read <$> getLine
   putStrLn $ show $ interest principal interestRate years
 
-interest :: Double -> Double -> Integer -> Double
 interest principal interestRate years =
   principal * ((1 + interestRate / 100) ^ years)
 
-{-
-main = do
-  principal    <- getLine >>= (\s -> read s :: IO Double)
-  interestRate <- getLine >>= (\s -> read s :: IO Double)
-  years        <- getLine >>= (\s -> read s :: IO Double)
-  putStrLn $ show $ interest principal interestRate years
-
--}
